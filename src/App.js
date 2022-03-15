700bfb676801038d5032d559c1235576bd54b572
kostyan <kostyan785@mail.ru>
2022-03-15 10:37:24 +0700

    Начальная кастомизация CRA
    Базовое приложение CRA кастомизировано, убраны лишние данные, информация заменена на актуальную
    Добавлен BrowserRouter, установлен пакет "react-router-dom"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+// Own
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+ * Компонент "ReactJS - Демо-приложение"
+ */
+
+// Экспорт компонента
+export const App = () => {
+    return (
+        <>ReactJS - Демо-приложение</>
+    )
 }
 
-export default App;
+// Дефолтный экспорт компонента
+export default App
